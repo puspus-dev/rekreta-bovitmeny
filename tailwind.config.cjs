3057a163417961e1186f774c585c0370853ad697
puspus-dev <boszormenyiadam03@gmail.com>
2025-10-10 09:28:03 +0200

    update1

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+theme: {
+extend: {
+colors: {
+rekreta: {
+50: '#f0fdf9',
+100: '#ccfbf1',
+200: '#99f6e4',
+300: '#5eead4',
+400: '#2dd4bf',
+500: '#10b981',
+600: '#059669',
+700: '#047857',
+800: '#065f46',
+900: '#064e3b'
+}
+},
+boxShadow: {
+'rk-md': '0 10px 30px rgba(2,6,23,0.12)'
+}
+}
+},
+plugins: []
+}
